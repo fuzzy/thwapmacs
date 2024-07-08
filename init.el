0fdff262b7e041c6f90bf279bfe7ae6dace9eeff
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2024-07-07 23:04:09 -0700

    catching things up

--- init.el
+++ init.el
@@ -4,6 +4,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(xterm-mouse-mode t)
+ '(mouse-autoselect-window t)
+ '(mouse-drag-and-drop-region t)
+ '(mouse-drag-and-drop-region-cross-program t)
  '(terraform-format-on-save t)
  '(terraform-indent-level 2)
  '(tool-bar-mode nil)
