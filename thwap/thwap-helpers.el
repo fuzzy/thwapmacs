7bdb2b06f931984c16d47e867b0b9fc7197cbc6f
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2024-07-10 01:52:22 -0700

    helper functions documentation

--- thwap/thwap-helpers.el
+++ thwap/thwap-helpers.el
@@ -3,7 +3,9 @@
 ;;
 
 (defun thwap/dashboard-insert-logo-title (banner)
-	"Insert BANNER into the dashboard buffer."
+	"Insert BANNER into the dashboard buffer.
+BANNER can be a single string, which will be centered, or a list of strings,
+which will be displayed with line breaks between them."
 	(insert "\n")
 	;; if banner is a single string, display it in the center
 	;; else if it is a list of strings, display them with line breaks
@@ -14,16 +16,24 @@
 			(insert "\n"))))
 
 (defun thwap/dashboard-build-logo-title (lst)
-	"Build a list of strings from LST to display as the banner."
+	"Build a list of strings from LST to display as the banner.
+LST is reversed and concatenated into a single string with line breaks."
 	(mapconcat 'identity (reverse lst) "\n"))
 
 (defun thwap/random-string-from-list (strings)
+	"Return a random string from STRINGS."
   (let ((index (random (length strings))))
     (nth index strings)))
 
 ;;
 ;; Helper functions for general use
 ;;
+
+;; Function to create a directory if it doesn't exist
+(defun thwap/ensure-directory-exists (dir)
+  "Ensure the directory DIR exists. If not, create it."
+  (unless (file-directory-p dir)
+    (make-directory dir t)))
 
 ;; Function to list all files in a directory with a given extension
 (defun thwap/list-files-with-extension (dir extension)
@@ -46,7 +56,10 @@
 
 ;; Function to update org-agenda-files
 (defun thwap/org-agenda-files-update ()
-  "Update the org-agenda-files variable."
+	"Update the `org-agenda-files` variable.
+This function sets `org-agenda-files` and `org-timeblock-files` to the list of
+all `.org` files in the `~/.org-agenda` directory, and sets
+`org-timeblock-inbox-file` to `~/.org-agenda/tasks.org`."
   (setq org-agenda-files (thwap/list-files-with-extension "~/.org-agenda" "org"))
   (setq org-timeblock-files (thwap/list-files-with-extension "~/.org-agenda" "org"))
   (setq org-timeblock-inbox-file "~/.org-agenda/tasks.org"))
