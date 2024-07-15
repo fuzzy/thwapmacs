750f8b73e514bab3b8147d529a9713475cb7bc7b
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2024-07-15 07:28:20 -0700

    prime the pump

---
+++ emacs-custom.el
@@ -0,0 +1,19 @@
+;; custom set stuff
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mouse-autoselect-window t)
+ '(mouse-drag-and-drop-region t)
+ '(mouse-drag-and-drop-region-cross-program t)
+ '(tool-bar-mode nil)
+ '(visible-bell 1)
+ '(xterm-mouse-mode t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Noto Sans Mono" :foundry "GOOG" :slant normal :weight regular :height 90 :width normal)))))
